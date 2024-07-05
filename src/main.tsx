96a09a463519743d2726f85996c7a50aa93ea6b6
Alexey Basov <alexey@basovbros.io>
2024-07-05 21:17:27 +0300

    add antd design lib

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import { Provider as StoreProvider } from "react-redux";
+import { ConfigProvider, theme } from "antd";
 
 import { App } from '@app';
 import { Router } from "@app-router";
 import { Store } from "@app-store";
 
+import "antd/dist/reset.css";
 import "@assets/scss/style.scss";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App>
-      <StoreProvider store={Store}>
-        <RouterProvider router={Router} />
-      </StoreProvider>
+      <ConfigProvider theme={{
+        algorithm: theme.darkAlgorithm
+      }}>
+        <StoreProvider store={Store}>
+          <RouterProvider router={Router} />
+        </StoreProvider>
+      </ConfigProvider>
     </App>
   </React.StrictMode>,
 )
