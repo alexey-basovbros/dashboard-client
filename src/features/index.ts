5a40395242c3a5b2f369314e1abd9f1ea3a68376
Alexey Basov <alexey@basovbros.io>
2024-07-05 22:38:43 +0300

    export addede features

---
+++ src/features/index.ts
@@ -0,0 +1,11 @@
+import { Sidebar } from "./sidebar";
+import { Navbar as PrivateNavbar } from "./navbar/private";
+import { Navbar as PublicNavbar } from "./navbar/public";
+import { Footer } from "./footer";
+
+export {
+  Footer,
+  Sidebar,
+  PublicNavbar,
+  PrivateNavbar
+};
